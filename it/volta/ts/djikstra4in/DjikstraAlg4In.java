691984184aaa94787d0fd7c622de48d4d8394c2f
Dante Del Favero <delfavero.dante@volta.ts.it>
2023-02-07 22:20:36 +0100

    added comment

--- it/volta/ts/djikstra4in/DjikstraAlg4In.java
+++ it/volta/ts/djikstra4in/DjikstraAlg4In.java
@@ -3,7 +3,8 @@ package it.volta.ts.djikstra4in;
 import it.volta.ts.djikstra4in.ui.Console;
 
 public class DjikstraAlg4In {
-
+    
+    // Added a comment
     public static void main(String[] args) {
         new Console(args).run();
     }
